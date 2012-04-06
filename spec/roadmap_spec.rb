89584bd5b0a31d0f5e7244b30e5108bbeb43970e
Matt Mendell <matthew.mendell@gmail.com>
2012-04-06 19:01:19 -0400

    Add a few more tests

--- spec/roadmap_spec.rb
+++ spec/roadmap_spec.rb
@@ -12,8 +12,11 @@ require 'gen_roadmap'
 # Given a schedule, calculate the next free day.
 # If a worker does not yet have a free date, then
 
+# Things I don't like:
+# * Worker names are hardcoded.
+
 describe "populate_start_dates" do
-  it "populates start_date for feature without start_date" do
+  it "populates initial start_date for one feature with no start_date" do
     features = [
       { 'name' => 'task1',
         'estimated_effort' => '1 day' }
@@ -22,5 +25,39 @@ describe "populate_start_dates" do
     populate_start_dates(features)
 
     features[0]['start_date'].should == Date.today.strftime("%B %e, %Y")
+  end
+
+  it "populates future start_date to worker2" do
+    features = [
+      { 'name' => 'task1',
+        'estimated_effort' => '1 day',
+        'start_date' => Date.today.strftime("%B %e %Y"),
+        'owner' => 'worker1' },
+      { 'name' => 'task2',
+        'estimated_effort' => '1 day' }
+    ]
+
+    populate_start_dates(features)
+
+    features[1]['start_date'].should == Date.today.strftime("%B %e, %Y")
+  end
+
+  it "populates future start_date to worker1" do
+    features = [
+      { 'name' => 'task1',
+        'estimated_effort' => '1 day',
+        'start_date' => Date.today.strftime("%B %e %Y"),
+        'owner' => 'worker1' },
+      { 'name' => 'task2',
+        'estimated_effort' => '1 day',
+        'owner' => 'worker2' },
+      { 'name' => 'task3',
+        'estimated_effort' => '1 day' }
+    ]
+
+    populate_start_dates(features)
+
+    # Takes account of weekend, this test only passes on Friday
+    features[2]['start_date'].should == (Date.today + 3).strftime("%B %e, %Y")
   end
 end
