9026f79a51de8da1a2c5bf6aa3a852e08b49b1ba
Matt Mendell <matthew.mendell@gmail.com>
2012-04-06 14:36:26 -0400

    Initial commit of dead simple roadmap generator

---
+++ gen_roadmap.rb
@@ -0,0 +1,84 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'erb'
+require 'csv'
+require 'ostruct'
+require 'date'
+
+def populate_start_dates(features)
+    day = DateTime.now
+    day_free = {}
+    day_free['worker1'] = day
+    day_free['worker2'] = day
+
+    features.each do |feat|
+        if !feat.has_key?('estimated_effort')
+            p "Topic #{feat['name']} does not have an estimated_effort"
+            raise NotImplementedError
+        end
+        estimated_effort = feat['estimated_effort']
+
+        if feat.has_key?('start_date')
+            if !feat.has_key?('owner')
+                p "Topic #{feat['name']} has a start_date, but no owner"
+                raise NotImplementedError
+            end
+
+            owner = feat['owner']
+            start_date = DateTime.parse(feat['start_date'])
+        else
+            # Pick the owner who is free first
+            if day_free['worker1'] < day_free['worker2']
+                owner = 'worker1'
+            else
+                owner = 'worker2'
+            end
+
+            start_date = day_free[owner]
+
+            # Update start_time
+            feat['start_date'] = start_date.strftime("%B %e, %Y")
+        end
+
+        if estimated_effort =~ /week/
+            effort_days = estimated_effort.match(/(?<week>\d+)/)[:week].to_i * 5
+        elsif estimated_effort =~ /day/
+            effort_days = estimated_effort.match(/(?<day>\d+)/)[:day].to_i
+        end
+
+        # Skip weekends
+        effort_days.times do
+            if start_date.to_date.wday == 5
+                # Friday => Monday
+                start_date += 3
+            else
+                start_date += 1
+            end
+        end
+
+        # May have earlier, longer task
+        if (start_date > day_free[owner])
+            day_free[owner] = start_date
+        end
+    end
+end
+
+features = YAML::load_file('features.yml')
+populate_start_dates(features)
+
+erb = ERB.new(File.read('roadmap.rhtml'))
+output = erb.result(binding)
+
+# Static HTML file to link to from wiki.
+File.open("roadmap.html", "w") do |f|
+  f.write(output)
+end
+
+# Create CSV file for managers to use
+CSV.open("roadmap.csv", "w") do |csv|
+  csv << ['name', 'owner', 'estimated_effort', 'start_date', 'end_date']
+  features.each do |f|
+    csv << [f['name'], f['owner'], f['estimated_effort'], f['start_date'], f['end_date']]
+  end
+end
